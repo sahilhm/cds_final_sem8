3feb53ecd2126f9d2784056b25c7ee592cfcd805
Sahil Modi <sahilmodi26113006@gmail.com>
2020-04-21 16:57:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-bucket-nikunj"
+    bucket = "yastee-terraform-bucket"
     key    = "terraform.tfstate"
     region = "us-east-1"
   }
@@ -17,9 +17,9 @@ data "template_file" "userdata_client" {
 resource "aws_instance" "c0m1" {
 	ami = "ami-0affd4508a5d2481b"
 	instance_type = "t2.micro"
-	key_name = "Project"
+	key_name = "Project_virginia"
 	user_data = "${data.template_file.userdata_client.rendered}"
-	security_groups = [ "project" ]
+	security_groups = [ "launch-wizard-3" ]
 	tags = {
   	Name="C0m1"
   }
@@ -29,9 +29,9 @@ resource "aws_instance" "c0m1" {
 resource "aws_instance" "idx-1" {
 	ami = "ami-0affd4508a5d2481b"
 	instance_type = "t2.micro"
-	key_name = "Project"
+	key_name = "Project_virginia"
 	user_data = "${data.template_file.userdata_client.rendered}"
-	security_groups = [ "project" ]
+	security_groups = [ "launch-wizard-3" ]
 	tags = {
   	Name="Idx-1"
   }
@@ -40,9 +40,9 @@ resource "aws_instance" "idx-1" {
 resource "aws_instance" "idx-2" {
 	ami = "ami-0affd4508a5d2481b"
 	instance_type = "t2.micro"
-	key_name = "Project"
+	key_name = "Project_virginia"
 	user_data = "${data.template_file.userdata_client.rendered}"
-	security_groups = [ "project" ]
+	security_groups = [ "launch-wizard-3" ]
 	tags = {
   	Name="Idx-2"
   }
@@ -51,9 +51,9 @@ resource "aws_instance" "idx-2" {
 resource "aws_instance" "sh" {
 	ami = "ami-0affd4508a5d2481b"
 	instance_type = "t2.micro"
-	key_name = "Project"
+	key_name = "Project_virginia"
 	user_data = "${data.template_file.userdata_client.rendered}"
-	security_groups = [ "project" ]
+	security_groups = [ "launch-wizard-3" ]
 	tags = {
   	Name="SH"
   }
@@ -63,9 +63,9 @@ resource "aws_instance" "sh" {
 resource "aws_instance" "uf" {
 	ami = "ami-0affd4508a5d2481b"
 	instance_type = "t2.micro"
-	key_name = "Project"
+	key_name = "Project_virginia"
 	user_data = "${data.template_file.userdata_client.rendered}"
-	security_groups = [ "project" ]
+	security_groups = [ "launch-wizard-3" ]
 	tags = {
   	Name="UF"
   }
@@ -73,15 +73,15 @@ resource "aws_instance" "uf" {
 }
 
 data "aws_eip" "static_ip_c0m1" {
-  public_ip = "3.220.194.182"
+  public_ip = "100.26.130.241"
 }
 
 data "aws_eip" "static_ip_idx-1" {
-  public_ip = "52.4.66.78"
+  public_ip = "18.205.122.37"
 }
 
 data "aws_eip" "static_ip_idx-2" {
-  public_ip = "54.89.77.140"
+  public_ip = "52.206.52.56"
 }
 
 resource "aws_eip_association" "static_ip_c0m1" {
