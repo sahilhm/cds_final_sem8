15a3a76d20ed4ce04b38e475d853ea83da47d658
vidhipalan <40350761+vidhipalan@users.noreply.github.com>
2020-04-26 15:19:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,9 +17,9 @@ data "template_file" "userdata_client" {
 resource "aws_instance" "c0m1" {
 	ami = "ami-0affd4508a5d2481b"
 	instance_type = "t2.micro"
-	key_name = "Project_virginia"
+	key_name = "Project_demo"
 	user_data = "${data.template_file.userdata_client.rendered}"
-	security_groups = [ "launch-wizard-3" ]
+	security_groups = [ "launch-wizard-1" ]
 	tags = {
   	Name="C0m1"
   }
@@ -29,9 +29,9 @@ resource "aws_instance" "c0m1" {
 resource "aws_instance" "idx-1" {
 	ami = "ami-0affd4508a5d2481b"
 	instance_type = "t2.micro"
-	key_name = "Project_virginia"
+	key_name = "Project_demo"
 	user_data = "${data.template_file.userdata_client.rendered}"
-	security_groups = [ "launch-wizard-3" ]
+	security_groups = [ "launch-wizard-1" ]
 	tags = {
   	Name="Idx-1"
   }
@@ -40,9 +40,9 @@ resource "aws_instance" "idx-1" {
 resource "aws_instance" "idx-2" {
 	ami = "ami-0affd4508a5d2481b"
 	instance_type = "t2.micro"
-	key_name = "Project_virginia"
+	key_name = "Project_demo"
 	user_data = "${data.template_file.userdata_client.rendered}"
-	security_groups = [ "launch-wizard-3" ]
+	security_groups = [ "launch-wizard-1" ]
 	tags = {
   	Name="Idx-2"
   }
@@ -51,9 +51,9 @@ resource "aws_instance" "idx-2" {
 resource "aws_instance" "sh" {
 	ami = "ami-0affd4508a5d2481b"
 	instance_type = "t2.micro"
-	key_name = "Project_virginia"
+	key_name = "Project_demo"
 	user_data = "${data.template_file.userdata_client.rendered}"
-	security_groups = [ "launch-wizard-3" ]
+	security_groups = [ "launch-wizard-1" ]
 	tags = {
   	Name="SH"
   }
@@ -63,9 +63,9 @@ resource "aws_instance" "sh" {
 resource "aws_instance" "uf" {
 	ami = "ami-0affd4508a5d2481b"
 	instance_type = "t2.micro"
-	key_name = "Project_virginia"
+	key_name = "Project_demo"
 	user_data = "${data.template_file.userdata_client.rendered}"
-	security_groups = [ "launch-wizard-3" ]
+	security_groups = [ "launch-wizard-1" ]
 	tags = {
   	Name="UF"
   }
@@ -73,15 +73,15 @@ resource "aws_instance" "uf" {
 }
 
 data "aws_eip" "static_ip_c0m1" {
-  public_ip = "100.26.130.241"
+  public_ip = "3.217.1.29"
 }
 
 data "aws_eip" "static_ip_idx-1" {
-  public_ip = "18.205.122.37"
+  public_ip = "54.152.4.170"
 }
 
 data "aws_eip" "static_ip_idx-2" {
-  public_ip = "52.206.52.56"
+  public_ip = "54.165.79.126"
 }
 
 resource "aws_eip_association" "static_ip_c0m1" {
